22a706ba2c8a5653aa841eb35a7b6dd25fc5076a
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-31 10:56:22 +0530

    Add files via upload

---
+++ AddUpto_singleDigit.java
@@ -0,0 +1,26 @@
+// Java program to find sum of digits of a number until sum becomes single digit.
+import java.util.*;
+
+public class AddUpto_singleDigit{
+	
+	static int digSum(int n)
+	{
+		int sum = 0;
+		while (n > 0 || sum > 9)
+		{
+			if (n == 0) {
+				n = sum;
+				sum = 0;
+			}
+			sum += n % 10;
+			n /= 10;
+		}
+		return sum;
+	}
+
+	public static void main(String argc[])
+	{
+		int n = 5214;
+		System.out.println(digSum(n));
+	}
+}
