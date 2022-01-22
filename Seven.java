606ec69d47e0c0794c89ff5e2664cff3a970d4e0
Sonu Swapan Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2022-01-22 20:20:28 +0530

    Add files via upload

---
+++ Seven.java
@@ -0,0 +1,25 @@
+public class Seven {
+    public static void main(String[] args) {
+       
+        for(int i=1;i<=4;i++){
+        System.out.print("* ");
+        }
+        System.out.println();
+        
+        for (int i = 1; i <=4; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("  ");
+
+            }
+            for (int k = 1; k <= i; k++) {
+                if (i >= 2 && k > 1) {
+                    System.out.print(" ");
+                } 
+                else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
